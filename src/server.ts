ed970f3c8ac0866c592155d61c161b109dd8f7d4
dimpho290 <dimphotshid1@gmail.com>
2025-09-11 01:43:44 +0200

    Created author model and in-memory array

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import { log } from "console"
+import bodyParser from "body-parser"
 import express,{Express} from "express"
+import { loggerMiddleware } from "./middleware/logger"
+import router from "./routes/authors"
 
 
 const app:Express = express()
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(bodyParser.json())
+
+app.use(loggerMiddleware)
+
+app.use("/v1/author" ,router)
 
 app.listen(PORT ,() =>{
   console.log(`Server is running on http://localhost: ${PORT}`)
